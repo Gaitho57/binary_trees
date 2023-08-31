049a28ed43da283b17fe4988cacdc2a72c534e5e
Gaitho57 <michaelgaitho10@gmail.com>
2023-09-01 02:26:26 +0300

    balance

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -18,7 +18,8 @@ int binary_tree_balance(const binary_tree_t *tree)
  * binary_tree_height - Measure of the height of a binary tree.
  * @tree: pointer to the root node of the tree to measure the height.
  *
- * Return: whether tree is NULL, your function must return 0, else return height.
+ * Return: whether tree is NULL, your function must return 0,
+ * else return height.
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
